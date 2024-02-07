668d2d566e11112937b30886cf403bb30dc6937f
Madhula Veronica J <madhula.veronica@gmail.com>
2024-02-07 12:50:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Appbar from './Components/Appbar';
+import Card from './Components/Card';
+import Geometry from './Components/Geometry';
+import Stats from './Components/Stats';
+import Operations from './Components/Operations';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Appbar />
+    <br>
+    </br>
+    <br>
+    </br>
+    <Card/>
+    <br>
+    </br>  
+    <br>
+    </br>    
+    <Geometry />
+    <br>
+    </br>
+    <Stats />
+    <br>
+    </br>
+    <Operations />
+    <br>
+    </br>
+    <Footer />
+    </>
   );
 }
 
